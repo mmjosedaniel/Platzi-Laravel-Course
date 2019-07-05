6c8cc4e6f40090fa5ab8802bc7571417ae4d03c1
mmjosedaniel <mmjosedaniel@hotmail.com>
2019-07-05 15:35:50 -0500

    Class 14. Form for reports

--- resources/views/expenseReport/index.blade.php
+++ resources/views/expenseReport/index.blade.php
@@ -1,16 +1,25 @@
 @extends('layouts.base')
 
 @section('content')
-<div class="row">
-    <div class="col">
-        <h1>Reports</h1>
-        <table>
-            @foreach ($expenseReports as $expenseReport)
-             <tr>
-             <td>{{ $expenseReport->title }}</td>
-             </tr>
-            @endforeach
-         </table>
+    <div class="row">
+        <div class="col">
+            <h1>Reports</h1>
+        </div>
     </div>
-</div>
+    <div class="row">
+        <div class="col">
+            <a class="btn btn-primary" href="/expense_reports/create">Create a new report</a>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col">
+            <table class="table">
+                @foreach($expenseReports as $expenseReport)
+                    <tr>
+                        <td>{{ $expenseReport->title }}</td>
+                    </tr>
+                @endforeach
+            </table>
+        </div>
+    </div>
 @endsection
